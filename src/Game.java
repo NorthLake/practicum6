85d1868cbee1b0799cc5962243509c9e8d4f7fa6
Martijn Noordermeer <martijn.noordermeer@gmail.com>
2020-04-02 12:20:21 +0200

    First commit

---
+++ src/Game.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Game {
+    private String naam;
+    private int releaseJaar;
+    private double nieuwprijs;
+
+    public Game(String nm, int rJ, double nwPr) {
+        this.naam = nm;
+        this.releaseJaar = rJ;
+        this.nieuwprijs = nwPr;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double huidigeWaarde() {
+        return Math.floor(nieuwprijs * Math.pow(0.7, LocalDate.now().getYear() - releaseJaar) * 100) / 100;
+    }
+
+    @Override
+    public boolean equals(Object andereObject) {
+        if (andereObject instanceof Game) {
+            Game game = (Game)andereObject;
+            return game.naam.equals(this.naam) &&
+                    game.releaseJaar == this.releaseJaar &&
+                    game.nieuwprijs == this.nieuwprijs;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return naam + ", uitgegeven in " + releaseJaar + "; nieuwprijs: €" + nieuwprijs + " nu voor: €" + huidigeWaarde();
+    }
+}
---
+++ src/Persoon.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Persoon {
+    private String naam;
+    private double budget;
+    private ArrayList<Game> games;
+
+    public Persoon(String nm, double bud) {
+        this.naam = nm;
+        this.budget = bud;
+        games = new ArrayList<Game>();
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public boolean koop(Game g) {
+        boolean gekocht = false;
+        double waarde = g.huidigeWaarde();
+        if (!games.contains(g) && budget > waarde) {
+            games.add(g);
+            budget -= waarde;
+            gekocht = true;
+        }
+        return gekocht;
+    }
+
+    public boolean verkoop(Game g, Persoon koper) {
+        double waarde = g.huidigeWaarde();
+        if (!koper.games.contains(g) && games.contains(g) && koper.budget > waarde) {
+            games.remove(g);
+            koper.games.add(g);
+            budget += g.huidigeWaarde();
+            koper.budget -= g.huidigeWaarde();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String result = naam + " heeft een budget van €" + String.format("%.2f", budget) + " en bezit de volgende games:";
+        for (Game g : games) {
+            result += "\n" + g;
+        }
+        return result;
+    }
+}
