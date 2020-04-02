669529637045642e8249d6e0d34b5b857902d4c3
Martijn Noordermeer <martijn.noordermeer@gmail.com>
2020-04-02 13:40:59 +0200

    Commit opdracht 1

--- src/Persoon.java
+++ src/Persoon.java
@@ -38,6 +38,15 @@ public class Persoon {
         return false;
     }
 
+    public Game zoekGameOpNaam(String naam) {
+        for (Game game : games) {
+            if (game.getNaam().equals(naam)) {
+                return game;
+            }
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         String result = naam + " heeft een budget van €" + String.format("%.2f", budget) + " en bezit de volgende games:";
